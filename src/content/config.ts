447bcb3f1df8649e029eb79709154e529a4c4cff
wanoo21 <ionprodan33@gmail.com>
2023-09-12 11:22:38 +0300

    feat: Content configs and more pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,37 @@
-import { defineCollection, z } from 'astro:content';
+import {defineCollection, reference, z} from 'astro:content';
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const authors = defineCollection({
+    type: 'content',
+    schema: z.object({
+        name: z.string(),
+        avatar: z.string().optional(),
+        occupation: z.string().optional(),
+        company: z.string().optional(),
+        email: z.string().email(),
+        twitter: z.string().url().optional(),
+        linkedin: z.string().url().optional(),
+        github: z.string().url().optional(),
+        layout: z.string().url().optional(),
+    }),
 });
 
-export const collections = { blog };
+const blog = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        date: z.coerce.date(),
+        tags: z.array(z.string()).default([]),
+        lastmod: z.coerce.date().optional(),
+        draft: z.boolean().default(false),
+        summary: z.string().optional(),
+        images: z.string().optional(),
+        authors: z.array(reference('authors')).default(['default']),
+        layout: z.string().optional(),
+        bibliography: z.string().optional(),
+        canonicalUrl: z.string().optional(),
+        // Add related posts
+        related: z.array(reference('blog')).default([]),
+    }),
+});
+
+export const collections = {blog, authors};
